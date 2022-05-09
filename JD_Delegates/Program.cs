e9dea8c1e02ce8069ff084df3bb52a89c566dbfd
ehtom <ehtom@DESKTOP-SS1Q4KR>
2022-05-09 11:00:35 -0400

    Add project files.

---
+++ JD_Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JD_Delegates
+{
+    internal class Program
+    {
+        delegate string MyDel(); 
+
+        static void Main(string[] args)
+        {
+            MyDel testDel = TestFunction;
+            testDel += TestFunction2;
+           
+            string result = testDel();
+
+            Console.WriteLine(result);
+        }
+        static string TestFunction()
+        {
+            return "Hello world";
+        }
+        static string TestFunction2()
+        {
+            return "Seech";
+        }
+
+    }
+}
